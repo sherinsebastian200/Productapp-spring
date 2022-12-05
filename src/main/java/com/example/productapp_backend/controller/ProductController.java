f5a181e80bc94869b6e5657d1832992b8c81c234
sherin200 <sherinsebastian21>
2022-12-05 18:01:03 +0530

    search

--- src/main/java/com/example/productapp_backend/controller/ProductController.java
+++ src/main/java/com/example/productapp_backend/controller/ProductController.java
@@ -18,6 +18,11 @@ public class ProductController {
     {
         return "Add products";
     }
+    @PostMapping("/search")
+    public String Searchproducts()
+    {
+        return "Products search page";
+    }
 
 
 
